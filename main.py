1c8ba723e5b4eee7ca5303f675e350258c0268ad
Halophilus <33325217+Halophilus@users.noreply.github.com>
2022-08-02 21:06:10 -0400

    Add means of finding smallest image, reducing an image size,  finding the max number of images, return max images for a given entry, and means of making image labels

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from msilib.schema import Control
 import os
 from os.path import isfile, join
+from PIL import Image, ImageDraw, ImageFont
 
-dirpath = 'C:\\Users\\Thomas.Henshaw001\\Development\\montague\\2022_Ben'
+dirpath = 'C:\\Users\\Thomas.Henshaw001\\Development\\montague\\2022_Ben\\ML_amp timecourse'
 
 def listify(mypath):
     listdir = []
@@ -35,6 +37,59 @@ def dictify(breakdownlist):
             dirdict[k][v][a].append(d)
     return dirdict
 
+def smallestImgDim(breakdownlist):
+    data = {'min_width':0,'min_height':0,'min_pix_ct':0,'file_path':""}
+    for k, v, a, d in breakdownlist:
+        with Image.open(d) as img:
+            width, height = img.size
+            if width*height > data['min_pix_ct'] or data['min_pix_ct'] == 0:
+                data['min_width'] = width
+                data['min_height'] = height
+                data['min_pix_ct'] = width*height
+                data['file_path'] = d
+    return data
+
+def ensmallenImg(smallestimgdim, newpath):
+    image = Image.open(newpath)
+    smallimage = image.resize((smallestimgdim['min_width'],smallestimgdim['min_height']))
+    smallimage.save(newpath)
+
+def getMaxImages(breakdowndict):
+    maximgdict = {}
+    groupslist = list(breakdowndict.keys())
+    for key in groupslist:
+        maximgdict.update(breakdowndict[key])
+    maxbyweek = {}
+    nameslist = list(maximgdict.keys())
+    for names in nameslist:
+        for items in maximgdict[names]:
+            if items not in maxbyweek:
+                maxbyweek[items] = [len(maximgdict[names][items])]
+            else:
+    for weeks in maxbyweek:
+        maxentry = {weeks: max(maxbyweek[weeks])}
+        maxbyweek.update(maxentry)
+    return maxbyweek
+   
+def makeBanner(dimensions, name):
+    width = dimensions['min_width']
+    height = dimensions['min_height']
+    myFont = ImageFont.truetype("Arial.ttf", 16)
+    background = Image.new(mode = "RGB", size = (width, height), color = 'white')
+    w, h = draw.textsize(name, font = myFont)
+    draw = ImageDraw.draw(background)
+    draw.text((((width - w)/2),(height - h)/2), name, fill = "white")
+    return background
 
 
-print(dictify(breakdown(listify(dirpath))))
+
+
+
+
+
+test1 = dictify(breakdown(listify(dirpath)))
+
+print(getMaxImages(test1))
+
+
+
